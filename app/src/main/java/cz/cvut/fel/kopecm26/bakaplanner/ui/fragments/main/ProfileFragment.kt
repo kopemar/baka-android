35a29ce667fea04f5af0ca7c39bce4fc069dc5f8
Martina Kopecka <xykopecka@gmail.com>
2020-12-07 00:45:47 +0100

    Restart app after sign out

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragments/main/ProfileFragment.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragments/main/ProfileFragment.kt
@@ -1,17 +1,15 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main
 
 import androidx.lifecycle.viewModelScope
-import com.jakewharton.processphoenix.ProcessPhoenix
-import com.orhanobut.logger.Logger
 import com.pixplicity.easyprefs.library.Prefs
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.FragmentProfileBinding
 import cz.cvut.fel.kopecm26.bakaplanner.networking.model.ResponseModel
 import cz.cvut.fel.kopecm26.bakaplanner.networking.model.SignOutModel
+import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.SplashActivity
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.UrlActivity
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.base.ViewModelFragment
 import cz.cvut.fel.kopecm26.bakaplanner.util.Constants
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.PrefsUtils
 import cz.cvut.fel.kopecm26.bakaplanner.util.ext.startActivity
 import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.ProfileViewModel
 import kotlinx.coroutines.delay
@@ -28,14 +26,18 @@ class ProfileFragment : ViewModelFragment<ProfileViewModel, FragmentProfileBindi
         viewModel.viewModelScope.launch {
             viewModel.signOut().run {
                 if (this is ResponseModel.SUCCESS<SignOutModel>){
-                    Logger.d(PrefsUtils.getPrefsStringOrNull(Constants.Prefs.USER))
                     Prefs.remove(Constants.Prefs.USER)
+                    delay(500)
+
+                    requireActivity().run {
+                        finishAffinity()
+                        startActivity<SplashActivity>()
+                    }
+
                 } else if (this is ResponseModel.ERROR<SignOutModel>) {
                     this.errorType?.messageRes?.let { showSnackBar(it) }
                 }
 
-                delay(500)
-                ProcessPhoenix.triggerRebirth(requireContext())
             }
 
         }
