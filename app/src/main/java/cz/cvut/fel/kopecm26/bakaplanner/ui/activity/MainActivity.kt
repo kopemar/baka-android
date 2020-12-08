6c25ae00ecd30ea1c4d744307305bb361b414d39
Martina Kopecka <xykopecka@gmail.com>
2020-12-08 15:35:21 +0100

    Rework MainActivity fragments

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
@@ -1,34 +1,47 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.activity
 
+import androidx.annotation.IdRes
+import com.orhanobut.logger.Logger
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.ActivityMainBinding
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.base.ViewModelActivity
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.HomeFragment
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.ProfileFragment
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.ScheduleFragment
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.setFragment
 import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.MainViewModel
 
 
-class MainActivity : ViewModelActivity<MainViewModel, ActivityMainBinding>(R.layout.activity_main, MainViewModel::class) {
-
-    private val homeFragment by lazy { HomeFragment() }
-    private val scheduleFragment by lazy { ScheduleFragment() }
-    private val profileFragment by lazy { ProfileFragment() }
+class MainActivity : ViewModelActivity<MainViewModel, ActivityMainBinding>(
+    R.layout.activity_main,
+    MainViewModel::class
+) {
+    private var selectedFragment = -1
 
     override fun initUi() {
-        setFragment(binding.frameLayout.id, HomeFragment())
-
+        setFragment(R.id.menu_home)
         binding.bnvMain.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu_home -> setFragment(binding.frameLayout.id, homeFragment)
-                R.id.menu_profile -> setFragment(binding.frameLayout.id, profileFragment)
-                R.id.menu_schedule -> setFragment(binding.frameLayout.id, scheduleFragment)
-            }
+            it.itemId.let(::setFragment)
             true
         }
     }
 
+    private fun setFragment(@IdRes id: Int) {
+        Logger.d(id)
+        if (selectedFragment == id) return
+        supportFragmentManager.beginTransaction()
+            .run { supportFragmentManager.findFragmentByTag(selectedFragment.toString())?.let { hide(it) } ?: this }
+            .run {
+                supportFragmentManager.findFragmentByTag(id.toString())?.let(::show)
+                    ?: this.add(binding.frameLayout.id, createFragment(id), id.toString())
+            }.commit()
+        selectedFragment = id
+    }
 
+    private fun createFragment(@IdRes id: Int) = when (id) {
+        R.id.menu_home -> HomeFragment()
+        R.id.menu_profile -> ProfileFragment()
+        R.id.menu_schedule -> ScheduleFragment()
+        else -> HomeFragment()
+    }
 
 }
