c66b92cd3d70297ad64898b83620de4b8a779bd6
Martina Kopecka <xykopecka@gmail.com>
2020-12-13 20:49:04 +0100

    Navigate to contracts fragment

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.ActivityMainBinding
-import cz.cvut.fel.kopecm26.bakaplanner.navigation.KeepStateNavigator
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.base.ViewModelActivity
 import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.MainViewModel
 
@@ -18,9 +17,9 @@ class MainActivity : ViewModelActivity<MainViewModel, ActivityMainBinding>(
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
-        navController.navigatorProvider.addNavigator(navigator)
-        navController.setGraph(R.navigation.main_navigation)
+//        val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+//        navController.navigatorProvider.addNavigator(navigator)
+//        navController.setGraph(R.navigation.main_navigation)
 
         binding.bnvMain.setupWithNavController(navController)
     }
