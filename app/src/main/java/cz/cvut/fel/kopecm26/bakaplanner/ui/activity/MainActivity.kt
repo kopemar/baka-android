a60839c8e751a54b798b92332ae4408e6ee2e52c
Martina Kopecka <xykopecka@gmail.com>
2020-12-13 12:38:39 +0100

    Use navigation instead

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/MainActivity.kt
@@ -1,12 +1,10 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.activity
 
-import androidx.annotation.IdRes
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.ActivityMainBinding
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.base.ViewModelActivity
-import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.HomeFragment
-import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.ProfileFragment
-import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main.ScheduleFragment
 import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.MainViewModel
 
 
@@ -14,32 +12,11 @@ class MainActivity : ViewModelActivity<MainViewModel, ActivityMainBinding>(
     R.layout.activity_main,
     MainViewModel::class
 ) {
-    private var selectedFragment = -1
 
     override fun initUi() {
-        setFragment(R.id.menu_home)
-        binding.bnvMain.setOnNavigationItemSelectedListener {
-            it.itemId.let(::setFragment)
-            true
-        }
-    }
-
-    private fun setFragment(@IdRes id: Int) {
-        if (selectedFragment == id) return
-        supportFragmentManager.beginTransaction()
-            .run { supportFragmentManager.findFragmentByTag(selectedFragment.toString())?.let(::hide) ?: this }
-            .run {
-                supportFragmentManager.findFragmentByTag(id.toString())?.let(::show)
-                    ?: this.add(binding.frameLayout.id, createFragment(id), id.toString())
-            }.commit()
-        selectedFragment = id
-    }
-
-    private fun createFragment(@IdRes id: Int) = when (id) {
-        R.id.menu_home -> HomeFragment()
-        R.id.menu_profile -> ProfileFragment()
-        R.id.menu_schedule -> ScheduleFragment()
-        else -> HomeFragment()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
     }
 
 }
