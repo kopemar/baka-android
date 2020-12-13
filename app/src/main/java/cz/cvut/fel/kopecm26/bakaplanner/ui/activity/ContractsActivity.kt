751e703e30ea74d96ce0083bae46155479de9625
Martina Kopecka <xykopecka@gmail.com>
2020-12-13 18:29:19 +0100

    Navigate to contracts

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/ContractsActivity.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/activity/ContractsActivity.kt
@@ -1,5 +1,6 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.activity
 
+import com.orhanobut.logger.Logger
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.ActivityContractsBinding
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.base.ViewModelActivity
@@ -8,5 +9,10 @@ import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.ContractsViewModel
 class ContractsActivity: ViewModelActivity<ContractsViewModel, ActivityContractsBinding>(R.layout.activity_contracts, ContractsViewModel::class) {
     override fun initUi() {
         setUpToolbar(binding.toolbar.toolbar)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Logger.d("Support navigate up")
+        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragments/main/ProfileFragment.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragments/main/ProfileFragment.kt
@@ -1,11 +1,9 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.main
 
 import androidx.lifecycle.Observer
-import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.FragmentProfileBinding
-import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.ContractsActivity
 import cz.cvut.fel.kopecm26.bakaplanner.ui.activity.SplashActivity
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragments.base.ViewModelFragment
 import cz.cvut.fel.kopecm26.bakaplanner.util.ext.startActivity
@@ -33,10 +31,13 @@ class ProfileFragment : ViewModelFragment<ProfileViewModel, FragmentProfileBindi
         viewModel.signedOut.observe(this, signedOutObserver)
 
         binding.btnUrl.setOnClickListener {
-            val directions: NavDirections = ProfileFragmentDirections.navigateToUrl()
-            findNavController().navigate(directions)
+            findNavController().navigate(ProfileFragmentDirections.navigateToUrl())
         }
-        binding.btnContracts.setOnClickListener { requireActivity().startActivity<ContractsActivity>() }
+
+        binding.btnContracts.setOnClickListener {
+            findNavController().navigate(ProfileFragmentDirections.navigateToContracts())
+        }
+
         binding.btnLogOut.setOnClickListener { viewModel.signOut() }
     }
 
