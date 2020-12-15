4e9d2990e2d6441769ba9b6be2bd0859b612d906
Martina Kopecka <xykopecka@gmail.com>
2020-12-15 20:58:16 +0100

    Distinguish shifts visually by time

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/networking/model/Shift.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/networking/model/Shift.kt
@@ -1,14 +1,12 @@
 package cz.cvut.fel.kopecm26.bakaplanner.networking.model
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.squareup.moshi.JsonClass
 import cz.cvut.fel.kopecm26.bakaplanner.R
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.dayOfWeek
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.fullDateShortDayOfWeek
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.hoursAndMinutes
-import cz.cvut.fel.kopecm26.bakaplanner.util.ext.isBefore
+import cz.cvut.fel.kopecm26.bakaplanner.util.ext.*
 
 @Entity
 @JsonClass(generateAdapter = true)
@@ -24,10 +22,11 @@ data class Shift(
 
     val dayOfWeek get() = start_time.dayOfWeek()
 
-    val state: ShiftState get() = when {
-        end_time.isBefore() -> ShiftState.PAST
-        start_time.isBefore() -> ShiftState.NOW
-        else -> ShiftState.FUTURE
+    val shiftTime: ShiftTime get() = when {
+        start_time.isMorning() -> ShiftTime.MORNING
+        start_time.isDay() -> ShiftTime.DAY
+        start_time.isEvening() -> ShiftTime.EVENING
+        else -> ShiftTime.NIGHT
     }
 }
 
@@ -36,9 +35,9 @@ data class ShiftResponse(
     val shifts: List<Shift>
 )
 
-enum class ShiftState(@DrawableRes val shiftIcon: Int) {
-    PLANNING(R.drawable.ic_shift_planning),
-    FUTURE(R.drawable.ic_shift_future),
-    NOW(R.drawable.ic_shift_now),
-    PAST(R.drawable.ic_shift_past)
+enum class ShiftTime(@DrawableRes val shiftIcon: Int, @ColorRes val colorRes: Int) {
+    MORNING(R.drawable.ic_weather_sunset_up, R.color.light_blue),
+    DAY(R.drawable.ic_weather_sunny, R.color.greeno),
+    EVENING(R.drawable.ic_weather_sunset_down, R.color.sunny_evenings),
+    NIGHT(R.drawable.ic_weather_night, R.color.text)
 }
--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/util/ext/DateUtils.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/util/ext/DateUtils.kt
@@ -4,8 +4,14 @@ import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 
-fun String.isBefore(date: ZonedDateTime = ZonedDateTime.now()): Boolean = ZonedDateTime.parse(this).isBefore(date)
+fun String.isBefore(date: ZonedDateTime = ZonedDateTime.now()): Boolean =
+    ZonedDateTime.parse(this).isBefore(date)
 
 fun String.isBefore(date: String) = isBefore(ZonedDateTime.parse(date))
+
+fun String.isMorning() = ZonedDateTime.parse(this).hour in (4..8)
+fun String.isDay() = ZonedDateTime.parse(this).hour in (8..12)
+fun String.isEvening() = ZonedDateTime.parse(this).hour in (12..16)
+fun String.isNight() = ZonedDateTime.parse(this).hour.let { it in (16..24) || it in (0..4) }
 
 fun LocalDate.weeksAfter(count: Long = 1) = plus(count, ChronoUnit.WEEKS)
