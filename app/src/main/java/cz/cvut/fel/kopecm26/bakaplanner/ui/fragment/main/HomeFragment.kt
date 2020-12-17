4f3f9b673a609328827e6ef8e4ba5d813d8e80ad
Martina Kopecka <xykopecka@gmail.com>
2020-12-17 09:10:15 +0100

    Open shift detail from Home widgets

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragment/main/HomeFragment.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragment/main/HomeFragment.kt
@@ -1,6 +1,7 @@
 package cz.cvut.fel.kopecm26.bakaplanner.ui.fragment.main
 
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.orhanobut.logger.Logger
 import cz.cvut.fel.kopecm26.bakaplanner.R
@@ -11,7 +12,10 @@ import cz.cvut.fel.kopecm26.bakaplanner.ui.adapter.BaseListAdapter
 import cz.cvut.fel.kopecm26.bakaplanner.ui.fragment.base.ViewModelFragment
 import cz.cvut.fel.kopecm26.bakaplanner.viewmodel.HomeViewModel
 
-class HomeFragment: ViewModelFragment<HomeViewModel, FragmentHomeBinding>(R.layout.fragment_home, HomeViewModel::class) {
+class HomeFragment : ViewModelFragment<HomeViewModel, FragmentHomeBinding>(
+    R.layout.fragment_home,
+    HomeViewModel::class
+) {
     init {
         Logger.d("New instance of HomeFragment")
     }
@@ -19,19 +23,41 @@ class HomeFragment: ViewModelFragment<HomeViewModel, FragmentHomeBinding>(R.layo
     override fun initUi() {
         viewModel.nextWeekDays.observe(viewLifecycleOwner, observer)
         binding.swipeRefresh.setOnRefreshListener { viewModel.refreshShifts() }
+
+        binding.currentShift.setOnClickListener {
+            viewModel.currentShift.value?.id?.let(::openShiftDetail)
+        }
+
+        binding.nextShift.setOnClickListener {
+            viewModel.nextShift.value?.id?.let(::openShiftDetail)
+        }
     }
 
-    private val observer by lazy { Observer<List<Shift>> {
-        binding.rvDateTime.layoutManager = LinearLayoutManager(binding.root.context)
-        binding.rvDateTime.adapter = BaseListAdapter<Shift>(
-            { layoutInflater, viewGroup, attachToRoot -> ListDayTimeBinding.inflate(layoutInflater, viewGroup, attachToRoot)},
-            { shift, binding, index -> run {
-                (binding as ListDayTimeBinding).shift = shift
-                binding.last = index == it.size - 1
-            } },
-            null,
-            { old, new -> old.id == new.id },
-            { old, new -> old == new }
-        ).apply { setItems(it) }
-    } }
+    private val observer by lazy {
+        Observer<List<Shift>> {
+            binding.rvDateTime.layoutManager = LinearLayoutManager(binding.root.context)
+            binding.rvDateTime.adapter = BaseListAdapter<Shift>(
+                { layoutInflater, viewGroup, attachToRoot ->
+                    ListDayTimeBinding.inflate(
+                        layoutInflater,
+                        viewGroup,
+                        attachToRoot
+                    )
+                },
+                { shift, binding, index ->
+                    run {
+                        (binding as ListDayTimeBinding).shift = shift
+                        binding.last = index == it.size - 1
+                    }
+                },
+                null,
+                { old, new -> old.id == new.id },
+                { old, new -> old == new }
+            ).apply { setItems(it) }
+        }
+    }
+
+    private fun openShiftDetail(shiftId: Int) {
+        findNavController().navigate(ScheduleFragmentDirections.navigateToShiftDetail(shiftId))
+    }
 }
