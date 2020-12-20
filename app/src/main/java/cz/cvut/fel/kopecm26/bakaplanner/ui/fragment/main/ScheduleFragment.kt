d3ffb95135dbce392573d7250bd502f9f7167eda
Martina Kopecka <xykopecka@gmail.com>
2020-12-20 13:39:57 +0100

    Style toolbar

--- app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragment/main/ScheduleFragment.kt
+++ app/src/main/java/cz/cvut/fel/kopecm26/bakaplanner/ui/fragment/main/ScheduleFragment.kt
@@ -3,6 +3,8 @@ package cz.cvut.fel.kopecm26.bakaplanner.ui.fragment.main
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.orhanobut.logger.Logger
 import cz.cvut.fel.kopecm26.bakaplanner.R
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.FragmentScheduleBinding
 import cz.cvut.fel.kopecm26.bakaplanner.databinding.ListShiftBinding
@@ -45,5 +47,13 @@ class ScheduleFragment : ViewModelFragment<ScheduleViewModel, FragmentScheduleBi
         binding.swipeRefresh.isRefreshing = true
         viewModel.shifts.observe(viewLifecycleOwner, observer)
         binding.swipeRefresh.setOnRefreshListener { viewModel.refreshShifts() }
+
+        binding.rvShifts.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                Logger.d("Scroll listener ${recyclerView.computeVerticalScrollOffset()}")
+
+                binding.mainToolbar.appBarLayout.elevation = if (recyclerView.computeVerticalScrollOffset() > 0) 6F else 0F
+            }
+        })
     }
 }
